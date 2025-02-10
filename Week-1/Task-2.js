154dc5f9f38f6089d8c5945f848425496c8e68f4
tirth2101 <tirth.rank@bacancy.com>
2025-02-10 13:31:41 +0530

    All Week Folders Created

--- Week-1/Task-2.js
+++ Week-1/Task-2.js
@@ -1,3 +1,20 @@
-console.log("Lionel Messi");
-console.log("Tirth");
-console.log("Tirth God");
+// Define an array of strings
+const words = ["hello", "world", "LionelMessi", "Suarez", "Neymar"];
+
+let i = 0;
+
+// Start the while loop to iterate through the array
+while (i < words.length) {
+    // Break the loop if the current word's length exceeds 8 characters
+    if (words[i].length > 8) {
+        break;
+    }
+
+    // Use forEach 
+    [words[i]].forEach(word => {
+        console.log(word + "!");
+    });
+
+    // Increment the index to move to the next word
+    i++;
+}
