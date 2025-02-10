0ff62159dd019b4e5831952999907a28e980de1b
tirth2101 <tirth.rank@bacancy.com>
2025-02-10 23:15:14 +0530

    Update Task-6.js

--- Week-1/Task-6.js
+++ Week-1/Task-6.js
@@ -7,7 +7,7 @@ console.log("Initial person object:", person);
 
 // Modifying properties of a const object 
 person.age = 22;
-console.log("Modified person object:", person
+console.log("Modified person object:", person);
 
 // Eg. 2 - Const with arrays
 const num = [1, 2, 3, 4];
