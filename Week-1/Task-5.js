05ea0f31bc1ef75160210f946d4477f62f68e0a0
tirth2101 <tirth.rank@bacancy.com>
2025-02-10 14:37:29 +0530

    Task-5 of Week-1 Done

---
+++ Week-1/Task-5.js
@@ -0,0 +1,40 @@
+// Eg. 1 - Hoisting with var
+console.log(varVariable); 
+var varVariable = "declared using var";
+console.log(varVariable); 
+
+// Eg. 2 - Hoisting with let
+let letVariable = "declared using let"; 
+console.log(letVariable); 
+
+// Eg. 3 - Function Hoisting
+hoistedFunction(); 
+
+function hoistedFunction() {
+    console.log("hoisted function");
+}
+
+// Eg. 4 - Hoisting within a function
+function testHoisting() {
+    console.log(innerVar); // Undefined (var is hoisted)
+    var innerVar = "Inside function";
+
+    let innerLet = "Inside function with let";
+    
+    console.log(innerVar); // "Inside function"
+    console.log(innerLet); // "Inside function with let"
+}
+testHoisting();
+
+// Eg. 5 - Hoisting within a block
+{
+    var blockVar = "Inside block with var"; 
+    
+    let blockLet = "Inside block with let"; 
+
+    console.log(blockVar); 
+    console.log(blockLet); 
+}
+
+console.log(blockVar);
+
