94840c8704c326d05bfa52c82223198fa8428e26
tirth2101 <tirth.rank@bacancy.com>
2025-02-10 14:42:30 +0530

    Task-4 of Week-1 Done

--- Week-1/Task-4.js
+++ Week-1/Task-4.js
@@ -1,4 +1,3 @@
-//Task-4
 // Eg. 1 - Function Scope vs Block Scope
 function scopeTest() {
     if (true) {
@@ -15,22 +14,18 @@ function scopeTest() {
 scopeTest();
 
 // Eg. 2 - Redeclaration and Reassignment
-
 var a = 10;
 var a = 20; // Allowed: var can be redeclared
 
-let b = 30;
-// let b = 40; // Error: let cannot be redeclared in the same scope
+let b = 30; // let b = 40; // Error: let cannot be redeclared in the same scope
 
-const c = 50;
-// c = 60; // Error: const cannot be reassigned
+const c = 50; // c = 60; // Error: const cannot be reassigned
 
 console.log(a); // 20
 console.log(b); // 30
 console.log(c); // 50
 
-// Example 3: Hoisting Behavior
-
+// Eg. 3 - Hoisting Behavior
 console.log(hoistedVar); // Undefined (var is hoisted)
 var hoistedVar = "Var used for Hoisting";
 
