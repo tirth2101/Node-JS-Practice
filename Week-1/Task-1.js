9206b93c0b4b6bd05f404bc4c4a903dbacbba595
tirth2101 <tirth.rank@bacancy.com>
2025-02-10 12:32:39 +0530

    All Week Folders Created

--- Week-1/Task-1.js
+++ Week-1/Task-1.js
@@ -1,3 +1,19 @@
-console.log("Lionel Messi");
-console.log("Tirth");
-console.log("Tirth God");
+// Define an array of numbers
+const num = [1,2,3,4,5,6,7,8];
+
+// Iterate through the array using a for loop
+for (let i = 0; i < num.length; i++) {
+    console.log("Before control flow:", num[i]); 
+
+    // Skip the iteration if the number is exactly 3
+    if (num[i] === 3) {
+        continue;
+    }
+
+    // Break the loop if the number is greater than 5
+    if (num[i] > 5) {
+        break;
+    }
+
+    console.log("After control flow:", num[i]); 
+}
