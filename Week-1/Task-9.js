149563b9f051c3d30b0acc6db966093f162f11ff
tirth2101 <tirth.rank@bacancy.com>
2025-02-10 16:30:52 +0530

    Task-9 of Week-1 Done

---
+++ Week-1/Task-9.js
@@ -0,0 +1,16 @@
+// Declare variables with different data types
+let age = 22; // Number
+let name = "Tirth"; // String
+let isStudent = true; // Boolean
+
+// Concatenation
+let greeting = "Good Morning, " + name + "!"; 
+console.log(greeting); 
+
+// Convert the age to a string
+let ageString = "Age: " + age.toString(); 
+console.log(ageString); 
+
+// Combine the boolean value with a string
+let studentString = "student: " + isStudent; 
+console.log(studentString); 
